eefb86f7597becbf883e57ae8e8da078e8af876f
Artem Kislitsyn <arcilite.kd@gmail.com>
2017-02-03 18:09:09 +0300

    add methosds to Linked List

--- InterviewTasks.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
+++ InterviewTasks.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -3,6 +3,26 @@
 import Foundation
 
 
+public struct LinkedListIterator<T: Equatable>: IteratorProtocol {
+  public typealias Element = Node<T>
+  
+  /// The current node in the iteration
+  private var currentNode: Element?
+  
+  private init(startNode: Element?) {
+    currentNode = startNode
+  }
+  
+  public mutating func next() -> LinkedListIterator.Element? {
+    let node = currentNode
+    currentNode = currentNode?.next
+    
+    return node
+  }
+}
+
+
+
 public class Node<T: Equatable> {
   typealias NodeType = Node<T>
   
@@ -19,10 +39,11 @@ public class Node<T: Equatable> {
 class LinkedList<T: Equatable>{
   
   public private (set)  var  count:Int = 0
- 
+  public typealias Iterator = LinkedListIterator<T>
   public typealias NodeType = Node<T>
+  public typealias Element = Node<T>
   
-  private var start: NodeType? {
+  var start: NodeType? {
     didSet {
       // Special case for a 1 element list
       if end == nil {
@@ -31,7 +52,7 @@ class LinkedList<T: Equatable>{
     }
   }
   
-  private var end: NodeType? {
+   var end: NodeType? {
     didSet {
       // Special case for a 1 element list
       if start == nil {
@@ -39,6 +60,14 @@ class LinkedList<T: Equatable>{
       }
     }
   }
+  
+  /// Create a new LinkedList
+  ///
+  /// - returns: An empty LinkedList
+  public init() {
+    
+  }
+
   
   public init<S: Sequence>(_ elements: S) where S.Iterator.Element == T {
     for element in elements {
@@ -108,38 +137,111 @@ class LinkedList<T: Equatable>{
   
   
 //  func backRecursiveIteration(block:(_ node: NodeType) throws -> NodeType?) {
-//    
-//     iterateItem(node: end,block: block)
+//      iterateItem(node: end,block: block)
 //   // return nil
 //  }
 //  
 //  
 //  
-//  func iterateItem(node:NodeType?,block:(_ node: NodeType) throws -> NodeType?){
+//  func iterateItem(node:NodeType?,block:(_ node: NodeType) throws -> NodeType?) rethrows -> NodeType? {
 //    
 //    if let previosNode = node?.previous {
 //     print("node value \(node?.value)")
 //    
 //    let value  = try block(node!)
 //      
-//    iterateItem(node:previosNode,block: block)
+//    try iterateItem(node:previosNode,block: block)
 //    }else{
 //      //return node
 //    }
 //    
 //  }
+  
+  
+  /// Remove a give node from the list
+  ///
+  /// - complexity: O(1)
+  /// - parameter node: The node to remove
+  public func remove(node: NodeType) {
+    let nextNode = node.next
+    let previousNode = node.previous
+    
+    if node === start && node === end {
+      start = nil
+      end = nil
+    }
+    else if node === start {
+      start = node.next
+    } else if node === end {
+      end = node.previous
+    } else {
+      previousNode?.next = nextNode
+      nextNode?.previous = previousNode
+    }
+    
+    count -= 1
+    assert(
+      (end != nil && start != nil && count >= 1) || (end == nil && start == nil && count == 0),
+      "Internal invariant not upheld at the end of remove"
+    )
+  }
+  
+  /// Remove a give node from the list at a given index
+  ///
+  /// - complexity: O(n)
+  /// - parameter atIndex: The index of the value to remove
+  public func remove(atIndex index: Int) {
+    precondition(index >= 0 && index < count, "Index \(index) out of bounds")
+    
+    // Find the node
+    let result = iterate {
+      if $1 == index {
+        return $0
+      }
+      return nil
+    }
+    
+    // Remove the node
+    remove(node: result!)
+  }
+  
+  
+  
+//  public func makeIterator() -> LinkedList.Iterator {
+//    let iterator =  LinkedListIterator<T>(startNode: nil)
+//    return iterator
+//  }
+
+  
+  
 
 }
+
+
+
 
 let list = LinkedList([1,2,3])
 
 let list2 = LinkedList([1,2,3])
 
-list.backRecursiveIteration(block:{ item in
- 
-  return item
-})
-  
+//list.backRecursiveIteration(block:{ item in
+// 
+//  return item
+//})
+
+var list3 = LinkedList<Int>()
+list3.appendNode(value: 10)
+list3.appendNode(value: 20)
+list3.appendNode(value: 30)
+
+//for node in list {
+//  print("\(node)")
+//}
+//
+//let values: [Int] = list.map {
+//  $0.value
+//}
+
 print()
 
 
